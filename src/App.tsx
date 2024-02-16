a0215034b8e81a5f05c20d5715d9811079e84f34
Daniel Santos <danielsantos@sysfar.com.br>
2024-02-16 01:24:50 -0300

    feat: criando componente de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from 'antd'
-
-function App() {
+import { LoginScreen } from './modules/login'
 
 
-  return (
-    <div>
-      <Button type="primary">Botão Primário</Button>
-      <Button type="default">Botão Padrão</Button>
-      <Button type="dashed">Botão Tracejado</Button>
-      <Button type="danger">Botão de Perigo</Button>
-      <Button type="link">Botão Link</Button>
-    </div>
-  )
+const App = () => {
+  return <LoginScreen />
 }
 
-export default App
+export { App };
