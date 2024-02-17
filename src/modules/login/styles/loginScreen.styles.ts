09ed42e84b91cb7d01a6a19c515121a393ddb329
Daniel Santos <danielsantos@sysfar.com.br>
2024-02-17 19:22:25 -0300

    feat: implementado logo na tela de login

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -7,4 +7,28 @@ export const BackgroundImage = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: -1;
+`;
+
+export const LogoImage = styled.img``;
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d9d9d9;
+  width: 100%;
+  height: 100vh;
+  max-width: 646px;
+  margin-left: auto;
+  margin-right: 0;
+`;
+
+export const LimitedContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: 498px;
+  margin-left: 22px;
+  margin-right: 22px;
 `;
